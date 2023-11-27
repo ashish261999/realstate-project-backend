0503ad937dc6dc80afc2db14dda3c1b778a2a3e3
ashish ranjan <ashishranjanmahi07@gmail.com>
2023-11-27 10:24:18 +0530

    sign-up created with fully functional

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ require("./conn/connect");
 const userRoute = require("./router/user_router");
 const authRoute = require("./router/auth_router");
 
+const cors = require('cors');
+
+
 
 
 
 const app= express();
+
 app.use(express.json());
+app.use(cors());
 
 app.listen(8080 ,()=>{
     console.log("server is running on port no 8080 !")
