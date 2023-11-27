8f17513f0d74221fe445dbba1b6250cecb62d3f4
ashish ranjan <ashishranjanmahi07@gmail.com>
2023-11-27 11:23:09 +0530

    create signin routes

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -1,6 +1,9 @@
 const User = require("../model/user_model");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const errorHandler = require("../util/error");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const Signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,4 +17,22 @@ const Signup = async (req, res, next) => {
   }
 };
 
+const Signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser =await User.findOne({email});
+   if(!validUser) return next(errorHandler(404, "User not found !"));
+   const validPasword  = bcrypt.compareSync(password, validUser.password);
+   if(!validPasword)  return next(errorHandler(401, "wrong credentials!"));
+   const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+   const {password: pass , ...rest} = validUser._doc;
+   res
+   .cookie("acces_token", token , {httpOnly: true})
+    .status(200)
+    .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = Signup;
+module.exports = Signin;
--- router/auth_router.js
+++ router/auth_router.js
@@ -1,4 +1,5 @@
 const Signup = require("../controllers/auth_controllers");
+const Signin = require("../controllers/auth_controllers");
 
 const router = require("express").Router();
 
@@ -6,5 +7,6 @@ const router = require("express").Router();
 
 
 router.post("/sign-up" , Signup);
+router.post("/sign-in", Signin );
 
 module.exports = router;
