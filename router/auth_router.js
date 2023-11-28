7acdf7740b4566003521eef45d682e745ebb8ddc
ashish ranjan <ashishranjanmahi07@gmail.com>
2023-11-28 13:25:29 +0530

    google auth router and controller created

--- router/auth_router.js
+++ router/auth_router.js
@@ -1,4 +1,4 @@
-const {Signup }= require("../controllers/auth_controllers");
+const {Signup, Google }= require("../controllers/auth_controllers");
 const {Signin} = require("../controllers/auth_controllers");
 
 const router = require("express").Router();
@@ -8,5 +8,6 @@ const router = require("express").Router();
 
 router.post("/sign-up" , Signup);
 router.post("/sign-in", Signin );
+router.post("/google" ,Google);
 
 module.exports = router;
